8961e00501de3e1c2d915d07a3974e4498aff1ce
albertusip <albertusip@gmail.com>
2026-01-29 08:59:33 +0700

    Initial Component

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#000000",
+        card: "#000000",
+        accent: "#3DD6C6",
+        muted: "#9CA3AF",
+      },
+    },
+  },
+  plugins: [],
+}
